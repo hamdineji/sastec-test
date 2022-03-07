bd4e82dc5fb0948adf2dcf88806b2182388ee79d
karimhajsalahG <nejihamdi9@gmail.com>
2022-03-07 14:29:25 +0100

    adding the header to the application

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import FrainageComponent from './frainage/frainage';
 import './App.css';
 import {Container , Row , Col} from "react-bootstrap"
 import header from "./assets/header.png"
+import logo from "./assets/logo.png"
+
 
 function App() {
   return (
@@ -18,11 +20,25 @@ function App() {
           {/* the toggle of the dark and light mode by H.N */}
           <Col md={4}><ColorSchemeToggle /></Col>
         </Row>
-        
+        {/* this is the header section by H.N */}
+        <Row><Col md={2}><img src={logo}/></Col>
+        <Col md={2}>L'entretien</Col>
+        <Col md={2}>la distrubition</Col>
+        <Col md={2}>la climatisation</Col>
+        <Col md={2}>le freinage</Col>
+        <Col md={2}>
+          <div className="form-group has-search">
+          <span className="fa fa-search form-control-feedback"></span>
+    <input type="text" className="form-control" placeholder="Search"/>
+  </div></Col>
+        </Row>
       </Container>
       <img src={header}/>
-      {/* the component FrainageComponent used in the app by H.N */}
+      {
+      /* the component FrainageComponent used in the app by H.N */}
+      <Container>
       <FrainageComponent/>
+      </Container>
       </header>
 
     </div>
